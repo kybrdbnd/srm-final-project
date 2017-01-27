b9f5b7f61b16559c78a05ff84c03a038bb6d440d
Pranav Puri <pranavpuri.p@hotmail.com>
2017-01-27 20:10:04 +0530

    practicing filtering

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import quandl
 
 quandl.ApiConfig.api_key = 'Hqu7HLNnBU4bxBU4jLaZ'
-data = quandl.get_table("ZACKS/FC", ticker="MSFT")
+data = quandl.get(["NSE/BHARTIARTL.2", "NSE/BHARTIARTL.3"],
+                  start_date="2016-10-31", end_date="2016-12-31")
 print(data)

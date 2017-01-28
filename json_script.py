ec4b939083270f6a0c5f6c5058502ffa2e7ecb64
kybrdbnd <pranavpuri.p@hotmail.com>
2017-01-28 12:35:58 +0530

    formatting data

--- json_script.py
+++ json_script.py
@@ -1,9 +1,14 @@
 import requests
 API_KEY = "Hqu7HLNnBU4bxBU4jLaZ"
-url = "https://www.quandl.com/api/v3/datasets/NSE/BHARTIARTL.json?api_key={0}".format(
-    API_KEY
-)
+url = "https://www.quandl.com/api/v3/datasets/NSE/BHARTIARTL.json?column_index=2&start_date=2015-12-31&end_date=2016-12-31&collapse=monthly&api_key=Hqu7HLNnBU4bxBU4jLaZ"
 # print(url)
 data = requests.get(url)
 parsed_data = data.json()
-print(parsed_data)
+# print(parsed_data)
+# print(len(parsed_data['dataset']))
+print("Name: {}\n".format(parsed_data['dataset']['name']))
+print("Data\t\tHigh")
+print("------------------------------------")
+for data in parsed_data['dataset']['data']:
+    print ("{}\t{}".format(data[0], data[1]))
+# print(parsed_data['dataset']['data'])

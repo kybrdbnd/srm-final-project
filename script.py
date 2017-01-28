2b9eaa3d996492fabbc372705566d414ab091b97
kybrdbnd <pranavpuri.p@hotmail.com>
2017-01-28 19:09:32 +0530

    graph functionality added

--- script.py
+++ script.py
@@ -1,7 +1,26 @@
 import quandl
 import requests
+import matplotlib.pyplot as plt
+
+# variables declare
+high_array = []
+date_array = []
 quandl.ApiConfig.api_key = 'Hqu7HLNnBU4bxBU4jLaZ'
 
+# functions declaration
+
+
+def plot_graph(date, high, start_date, end_date):
+    plt.xlabel("Year")
+    plt.ylabel("High Value")
+    title = "High Value from {} to {}".format(start_date, end_date)
+    plt.title(title)
+    plt.plot(date, high)
+    # plt.axis([0, date_array, 0, high_array])
+    plt.show()
+
+
+# script code
 try:
     url = "https://www.quandl.com/api/v3/datasets/NSE/BHARTIARTL/metadata.json"
     try:
@@ -18,7 +37,10 @@ try:
         print("Date\t\t\tHigh\tLow\t")
         print("----------------------------------")
         for data in searched_data:
+            high_array.append(data[1])
+            date_array.append(data[0])
             print("{}\t{}\t{}\n".format(data[0], data[1], data[2]))
+        plot_graph(date_array, high_array, user_start_date, user_end_date)
     except Exception:
         print("Please Enter the dates in correct format")
 except Exception:
